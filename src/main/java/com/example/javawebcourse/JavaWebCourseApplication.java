65d0b7e24e9bb3238d6b9fc976dd9e395a8f2d1a
Misha <misha1tigr@gmail.com>
2024-12-16 22:34:12 +0200

    Added base code

---
+++ src/main/java/com/example/javawebcourse/JavaWebCourseApplication.java
@@ -0,0 +1,13 @@
+package com.example.javawebcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWebCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javawebcourse/JavaWebCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javawebcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
